6cc545f6952c6cc9525c99b3b6adde0073b14b6f
Moch Asef Saefudin <57564538+fesaworld@users.noreply.github.com>
2023-03-19 02:32:27 +0700

    fix(loading) : add function to detect network status when app openned
    redirects to the 'no internet' view when opening the application without being connected to a network. using the connectivity_plus package

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:jds_weather/screen/splashscreen/splash_screen.dart';
 
 import 'binding/global_binding.dart';
+import 'const/text_style.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   GlobalBinding().dependencies();
-  runApp(const MyApp());
+
+  var connectivityResult = await (Connectivity().checkConnectivity());
+  print('object');
+  print(connectivityResult);
+  if (connectivityResult == ConnectivityResult.none) {
+    runApp(const NoInternet());
+  } else {
+    runApp(const MyApp());
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -24,6 +34,42 @@ class MyApp extends StatelessWidget {
             fontFamily: GoogleFonts.karla().fontFamily
         ),
         home: const SplashScreen()
+    );
+  }
+}
+
+class NoInternet extends StatelessWidget {
+  const NoInternet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: false,
+        fontFamily: GoogleFonts.karla().fontFamily,
+      ),
+      home: Scaffold(
+        body: Container(
+          height: Get.height,
+          width: Get.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(Icons.wifi_off,size: 100,),
+              Text(
+                'No Internet',
+                style: title.copyWith(fontSize: 25),
+              ),
+              Text(
+                '"connect to network to open this app"',
+                style: subTitle.copyWith(fontSize: 18),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
