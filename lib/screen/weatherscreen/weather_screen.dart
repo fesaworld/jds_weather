d8db1fbdb27f340391cdd34d79c4613a11d0c53d
Moch Asef Saefudin <57564538+fesaworld@users.noreply.github.com>
2023-03-19 00:58:55 +0700

    build(weatherscreen) : display city, current date, greeting, temp, and weather
    display city, current date, greeting, temp, and weather in the first section in screen

--- lib/screen/weatherscreen/weather_screen.dart
+++ lib/screen/weatherscreen/weather_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:jds_weather/screen/weatherscreen/weather_controller.dart';
 
 import '../../const/color_pallete.dart';
+import '../../const/text_style.dart';
 
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({Key? key}) : super(key: key);
@@ -30,6 +32,86 @@ class WeatherScreen extends StatelessWidget {
                       child: SingleChildScrollView(
                         controller: controller.scrollController,
                         physics: AlwaysScrollableScrollPhysics(parent: BouncingScrollPhysics()),
+                        child: Column(
+                          children: [
+                            SizedBox(height: 30,),
+                            Container(
+                              height: 140,
+                              width: Get.width,
+                              child: Column(
+                                children: [
+                                  Text(
+                                    controller.weatherNowModel!.name
+                                        .toString(),
+                                    style: title.copyWith(fontSize: 25),
+                                  ),
+                                  Text(
+                                      DateFormat.yMMMMEEEEd()
+                                          .format(DateTime.now())
+                                          .toString(),
+                                      style: subTitle),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                    crossAxisAlignment:
+                                    CrossAxisAlignment.center,
+                                    children: [
+                                      Column(
+                                        crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                        mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                        children: [
+                                          Text(
+                                              'Hi. ${controller.greetingsGet(controller.name!)}'),
+                                          SizedBox(
+                                            height: 5,
+                                          ),
+                                          Text(
+                                            '${controller.weatherNowModel!.main!.temp!} \u2103',
+                                            style: title.copyWith(
+                                                fontSize: 30),
+                                          ),
+                                        ],
+                                      ),
+                                      Column(
+                                        crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                        mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                        children: [
+                                          Text(controller.weatherNowModel!
+                                              .weather![0].main
+                                              .toString(),
+                                              style: title),
+                                          SizedBox(
+                                              height: 50,
+                                              width: 75,
+                                              child:  Image.network(
+                                                'http://openweathermap.org/img/w/${controller.weatherNowModel!.weather![0].icon}.png',
+                                                fit: BoxFit.cover,
+                                                loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+                                                  if (loadingProgress == null) {
+                                                    return child;
+                                                  }
+                                                  return Center(
+                                                    child: CircularProgressIndicator.adaptive(),
+                                                  );
+                                                },
+                                              )
+                                          )
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                     Padding(
