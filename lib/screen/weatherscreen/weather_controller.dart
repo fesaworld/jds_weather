1dcd07aac6ee21f8db403c285748ba7d96ddce9b
Moch Asef Saefudin <57564538+fesaworld@users.noreply.github.com>
2023-03-19 00:38:16 +0700

    build(weatherscreen) : add backround and back button
    add backgorund image and make back button function to hide when when the display is scrolled

--- lib/screen/weatherscreen/weather_controller.dart
+++ lib/screen/weatherscreen/weather_controller.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
 import 'package:jds_weather/data/model/weather_now_model.dart';
 
@@ -9,6 +11,9 @@ class WeatherController extends BaseController {
   WeatherNowModel? weatherNowModel;
   Weather5DayModel? weather5dayModel;
 
+  bool buttonVisible = true;
+  ScrollController scrollController = ScrollController();
+
   @override
   void onInit() {
     // TODO: implement onInit
@@ -17,12 +22,27 @@ class WeatherController extends BaseController {
     name = Get.arguments[0];
     weatherNowModel = Get.arguments[1];
     weather5dayModel = Get.arguments[2];
+
+    scrollController.addListener(() {
+      if(scrollController.position.userScrollDirection == ScrollDirection.reverse){
+        buttonVisible = false;
+      }
+      // if(scrollController.position.userScrollDirection == ScrollDirection.forward){
+      if (scrollController.position.pixels == 0) {
+        buttonVisible = true;
+      }
+      // }
+      update();
+    });
   }
 
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
+
+    scrollController.dispose();
+    scrollController.removeListener(() { });
   }
 
 }
--- lib/screen/weatherscreen/weather_screen.dart
+++ lib/screen/weatherscreen/weather_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:jds_weather/screen/weatherscreen/weather_controller.dart';
 
+import '../../const/color_pallete.dart';
+
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({Key? key}) : super(key: key);
 
@@ -11,8 +13,43 @@ class WeatherScreen extends StatelessWidget {
         init: WeatherController(),
         builder: (controller) {
           return Scaffold(
-            body: Center(
-              child: Text('This screen 2'),
+            body: SafeArea(
+              child: Container(
+                height: Get.height,
+                width: Get.width,
+                decoration: const BoxDecoration(
+                    color: Colors.blueAccent,
+                    image: DecorationImage(
+                        image: AssetImage('assets/image/background.jpg'),
+                        fit: BoxFit.fitHeight)),
+                child: Stack(
+                  children: [
+                    Container(
+                      height: Get.height,
+                      width: Get.width,
+                      child: SingleChildScrollView(
+                        controller: controller.scrollController,
+                        physics: AlwaysScrollableScrollPhysics(parent: BouncingScrollPhysics()),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Visibility(
+                        visible: controller.buttonVisible,
+                        child: GestureDetector(
+                          child: const Icon(
+                            Icons.arrow_back,
+                            color: ColorPalette.body,
+                          ),
+                          onTap: () {
+                            Get.back();
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           );
         });
