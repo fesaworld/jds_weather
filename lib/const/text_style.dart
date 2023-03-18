0f02649ff065ea6aa299dd64342851f8b9f0ba06
Moch Asef Saefudin <57564538+fesaworld@users.noreply.github.com>
2023-03-18 16:04:49 +0700

    build(const) : make const color & text style
    make const color and text tyle to simplyfiy the selection of color and text style

---
+++ lib/const/text_style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'color_pallete.dart';
+
+TextStyle title = GoogleFonts.karla(color: ColorPalette.body, fontSize: 16, fontWeight: FontWeight.w700);
+TextStyle subTitle = GoogleFonts.karla(color: ColorPalette.body, fontSize: 14, fontWeight: FontWeight.w400);
